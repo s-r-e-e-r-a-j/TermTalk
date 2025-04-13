ab7f492fdc080cae9d5345ce36e9e5398bf03aea
Sreeraj <154326486+s-r-e-e-r-a-j@users.noreply.github.com>
2025-04-13 15:15:21 +0530

    Update client.py

--- client.py
+++ client.py
@@ -22,7 +22,7 @@ def banner():
 def receive_messages(sock):
     while True:
         try:
-            msg = sock.recv(3024).decode()
+            msg = sock.recv(2048).decode()
             if not msg:
                 raise ConnectionError
             print(msg)
